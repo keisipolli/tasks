f99577b486b0758c3c9fe76a29d42327554fd1db
Keisi Polli <keisi.polli@khk.ee>
2021-10-26 14:58:41 +0300

    Delete single task from tasks list

---
+++ app.js
@@ -0,0 +1,54 @@
+// event elements
+const form = document.querySelector('form');
+const taskList = document.querySelector('.collection');
+const deleteTasksBtn = document.querySelector('#delete-tasks');
+
+// events
+form.addEventListener('submit', addTask);
+taskList.addEventListener('click', deleteTask);
+deleteTasksBtn.addEventListener('click', deleteTasks);
+
+function deleteTasks(){
+    // event.target.previousElementSibling.innerHTML = '';
+    // taskList.innerHTML = '';
+    while (taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+function deleteTask(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Do you want to delete this task?')){
+            event.target.parentElement.remove();
+        }
+    }
+}
+
+function addTask(event){
+    // get form input data
+    const taskInput = document.querySelector('#task');
+    let task = taskInput.value;
+    // create <li> element
+    const li = document.createElement('li');
+    // add css class
+    li.className = 'collection-item';
+    // create text element
+    const text = document.createTextNode(task);
+    // add text to <li>
+    li.appendChild(text);
+    // create <a> element
+    const link = document.createElement('a');
+    // add css class
+    link.className = 'secondary-content';
+    // set href atribute to <a>
+    link.setAttribute('href', '#');
+    // add text content to <a>
+    link.appendChild(document.createTextNode('X'));
+    // add <a> to <li>
+    li.appendChild(link);
+    // add li to ul
+    const ul = document.querySelector('.collection');
+    ul.appendChild(li);
+    taskInput.value = '';
+    event.preventDefault();
+}
